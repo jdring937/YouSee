01863ccafeb2938634560102b6cff3f017fa17b8
jdring937 <ringjy@mail.uc.edu>
2018-01-29 18:03:06 -0500

    Add function to get lat long

--- YouSee/YouSee/MainPage.xaml.cs
+++ YouSee/YouSee/MainPage.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿//YT GeoLocation tutorial: https://www.youtube.com/watch?v=pH1WaO-5LDk
+//Drawing lines between two points: https://stackoverflow.com/questions/13433648/draw-a-line-between-two-point-on-a-google-map-using-jquery
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.Geolocator;
 
 namespace YouSee
 {
@@ -12,6 +16,25 @@ namespace YouSee
 		public MainPage()
 		{
 			InitializeComponent();
+            btnGetLocation.Clicked += BtnGetLocation_Clicked;
 		}
-	}
+
+        private async void BtnGetLocation_Clicked(object sender, EventArgs e)
+        {
+            await RetrieveLocation();
+        }
+
+        private async Task RetrieveLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 20;
+            TimeSpan span = new TimeSpan(0, 0, 0, 0, 60000);
+            var position = await locator.GetPositionAsync(timeout: span);
+
+            txtLat.Text = "Latitude: " + position.Latitude.ToString();
+            txtLong.Text = "Longitude: " + position.Longitude.ToString();
+
+
+        }
+    }
 }
