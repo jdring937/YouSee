71eb3ee14a7d1c5abf3a0566c863702819e1e141
Brad <bruceba@mail.uc.edu>
2018-01-29 20:24:09 -0500

    Geolocation Class
    Replaced Geolocation Class in main method with modular class called GeoLocation.cs, that has 2 methods. RetreiveLatitude() and RetriveLongitude(). Also tested the use of getter and setter of Latitude and Longitude in newly created Connect.cs Class

---
+++ YouSee/YouSee/Connnect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouSee
+{
+    class Connnect
+    {
+
+        //Server method:
+
+        public static void ServerConnect()
+        {
+            GeoLocation test = new GeoLocation();
+            double v = test.Latitude;
+
+
+
+        }
+
+
+
+        //Client method:
+
+        public static void ClientConnect()
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ YouSee/YouSee/GeoLocation.cs
@@ -0,0 +1,59 @@
+﻿using Plugin.Geolocator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouSee
+{
+    class GeoLocation
+    {
+        //private varible for latitude
+        public double Latitude {
+            get
+            {
+                return Latitude;
+            }
+            set
+            {
+                Latitude = value;
+            }
+        }
+
+        //Private varible for longitude
+        public double Longitude {
+            get
+            {
+                return Longitude;
+            }
+            set
+            {
+                Longitude = value;
+            }
+        }
+
+
+        //method to retrive Latitude from Current location
+        private async void RetrieveLatitude()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 5;
+            TimeSpan span = new TimeSpan(0, 0, 0, 0, 10000);
+            var position = await locator.GetPositionAsync(timeout: span);
+
+            Latitude = position.Latitude;
+        }
+
+        //method to retrive Longitude from Current location
+        private async void RetrieveLongitude()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 5;
+            TimeSpan span = new TimeSpan(0, 0, 0, 0, 10000);
+            var position = await locator.GetPositionAsync(timeout: span);
+
+            Longitude = position.Longitude;
+        }
+
+    }//end class
+
+} //end namespace
