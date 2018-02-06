6d7e99f5d457da32896bc17509cda86edb0846c1
Jake Reilman <reilmajb@mail.uc.edu>
2018-02-05 20:03:19 -0500

    Added maps thing

--- YouSee/YouSee.Android/MainActivity.cs
+++ YouSee/YouSee.Android/MainActivity.cs
@@ -20,6 +20,7 @@ namespace YouSee.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App());
         }
     }
